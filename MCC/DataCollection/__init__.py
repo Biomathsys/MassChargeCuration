7fcd84adb3bb5fa8a99eb3b3c94e9dcbd7146f44
cb-Hades <81743695+cb-Hades@users.noreply.github.com>
2024-06-24 16:32:21 +0200

    test #8

--- MCC/DataCollection/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['DataCollection']
-
-from . import DataCollection
--- MCC/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import Balancing
-from . import DataCollection
-from . import ReportGeneration
-
-from .core import * 
-from .util import *
